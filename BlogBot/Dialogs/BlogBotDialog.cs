ca4902d466f1227aa9a4e70570d00c8a7ca1e2d5
Rahul Rai <rahulrai@live.com>
2017-06-01 20:41:18 +1000

    pending work

---
+++ BlogBot/Dialogs/BlogBotDialog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogBot.Dialogs
+{
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    using BlogBot.Models;
+
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.FormFlow;
+
+    public class BlogBotDialog
+    {
+        public static readonly IDialog<string> dialog = Chain.PostToChain().Select(msg => msg.Text)
+            .Switch(
+            new RegexCase<IDialog<string>>(new Regex("^hi", RegexOptions.IgnoreCase),
+                (context, text) =>
+                    {
+                        return Chain.ContinueWith(new HelloDialog(), ContinueConversation);
+                        } ),
+            new DefaultCase<string, IDialog<string>>(
+                (context, text) =>
+                    {
+                        return Chain.ContinueWith(
+                            FormDialog.FromForm<BlogSearch>(BlogSearch.BuildForm(), FormOptions.PromptInStart),
+                            ContinueConversation);
+                    })
+            );
+
+        private static Task<IDialog<object>> ContinueConversation(IBotContext context, IAwaitable<object> item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlogBot/Models/BlogSearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogBot.Models
+{
+    using Microsoft.Bot.Builder.FormFlow;
+
+    public enum BlogAspectOptions
+    {
+        Article,
+        Profile,
+        Subscribe
+    }
+
+    [Serializable]
+    public class BlogSearch
+    {
+        public BlogAspectOptions? BlogAspect;
+        public string MessageForRahul;
+
+        public static IForm<BlogSearch> BuildForm()
+        {
+            return new FormBuilder<BlogSearch>()
+                .Message("Hi, I am Blog Bot.")
+                .Build();
+        }
+
+    }
+}
